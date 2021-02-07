a01c90cf0d2792c0cdeb947f066704ff82542961
Kristine <kreaselee@gmail.com>
2021-02-07 11:58:07 -0800

    Modified font and colors

--- app/src/main/java/com/example/homework1/SecondActivity.java
+++ app/src/main/java/com/example/homework1/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.homework1;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,6 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
 import org.json.JSONArray;
 import org.json.JSONException;
